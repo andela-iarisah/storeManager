180495d1edb185e8205eb18fbf5ae48e93ad1d1f
Ijeoma <gift_ij2002@yahoo.com>
2014-10-07 10:48:23 +0100

    initial commit

---
+++ app/models/item.server.model.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Item Schema
+ */
+var ItemSchema = new Schema({
+	category: {
+		type: String,
+		default: '',
+		required: 'Please select a category'
+	},
+	itemName: {
+		type: String,
+		default: '',
+		required: 'Please fill item name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	itemQuantity: {
+		type: Number,
+		default: 0,
+		required: 'Please fill item quantity'
+	},
+	minItemQuantity: {
+		type: Number,
+		default: 0,
+		required: 'Please fill the minimum item quantity you wish to have'
+	},
+	addedQuantity: {
+		type: Number,
+		default: 0
+	}
+});
+
+mongoose.model('Item', ItemSchema);
